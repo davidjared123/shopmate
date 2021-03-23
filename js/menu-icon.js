6baa76a8b2273f96e1a210995c0861ac398bc9f7
David Perez <davidjared123@gmail.com>
2021-03-22 21:35:10 -0400

    menu responsive

--- js/menu-icon.js
+++ js/menu-icon.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
-	$('.icon-menu').click(function(){
-		$(this).toggleClass('open');
-	});
-});
-
 function menuIcon() {
     document.querySelector('.icon-menu').classList.toggle('open')
+
+    const check = document.querySelector('#hamburger')
+    const menuMobile = document.querySelector('.navbar-menu')
+    if(check.checked){
+        menuMobile.style.right = "-50%"
+    } else(
+        menuMobile.style.right = "0"
+    )
 }
+
+
